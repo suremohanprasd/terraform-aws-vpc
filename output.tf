b9efc332b4fd9e9766c6791be36a5e6e16d62ec9
mohan <mohanprasad390@gmail.com>
2025-12-02 16:21:18 +0530

    message

--- output.tf
+++ output.tf
@@ -1,3 +1,15 @@
 output "vpc_id" {
     value = aws_vpc.main.id
 }
+
+output "public_subnet_ids" {
+    value = aws_subnet.public[*].id
+}
+
+output "private_subnet_ids" {
+    value = aws_subnet.private[*].id
+}
+
+output "datbase_subnet_ids" {
+    value = aws_subnet.database[*].id
+}
